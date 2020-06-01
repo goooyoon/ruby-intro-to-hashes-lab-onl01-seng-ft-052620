d49be145d05f7278df05af4ec392ba60a2aec8de
Grace Kim <goooyoon@gmail.com>
2020-06-01 17:13:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,14 +9,14 @@ end
 
 def monopoly
 	monopoly = {
-	  railroads=> {}
+	  :railroads => {}
 	}
 
 end
 
 def monopoly_with_second_tier
 	monopoly = {
-		:railroads=>{:pieces=>4,:names=>{},:rent_in_dollars=>{}
+		:railroads => {:pieces=>4,:names=>{},:rent_in_dollars=>{}
 		}
 	}
 end
